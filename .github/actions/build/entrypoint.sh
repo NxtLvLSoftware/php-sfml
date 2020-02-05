3a33307b10e4268ea5c0a2a7858e6b6f3e5afa87
JackNoordhuis <jackjnoordhuis@gmail.com>
2020-02-06 02:35:23 +1100

    fix build action dockerfile (install and enable extension after building)

--- .github/actions/build/entrypoint.sh
+++ .github/actions/build/entrypoint.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
-sh -c "phpize && ./configure && make -j $(nproc) && make test"
+if [ $# -eq 0 ]; then
+  sh -c "phpize && ./configure && make -j $(nproc) && make install && docker-php-ext-enable sfml && make test"
+else
+  exec "$@"
+fi
