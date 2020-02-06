0749a897af5dd76d09ccd3044e4999cfa0c7d2d2
JackNoordhuis <jackjnoordhuis@gmail.com>
2020-02-06 12:10:19 +1100

    build action: try forwarding env vars

--- .github/actions/build/entrypoint.sh
+++ .github/actions/build/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 if [ $# -eq 0 ]; then
-  sh -c "phpize && ./configure && make -j $(nproc) && make install && docker-php-ext-enable sfml && REPORT_EXIT_STATUS=1 NO_INTERACTION=1 TEST_PHP_ARGS="--show-diff" make test"
+  sh -c "phpize && CFLAGS=\"$CFLAGS\" CXXFLAGS=\"$CXXFLAGS\" ./configure && make -j $(nproc) && make install && docker-php-ext-enable sfml && REPORT_EXIT_STATUS=1 NO_INTERACTION=1 TEST_PHP_ARGS="--show-diff" make test"
 else
   exec "$@"
 fi
